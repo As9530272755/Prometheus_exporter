4071620798dd52c639eaf78db4a51d5e36379d10
Code:Z <58386845+As9530272755@users.noreply.github.com>
2021-10-09 14:25:15 +0800

    Update link.go

--- mysql_exporter/linkmysql/link.go
+++ mysql_exporter/linkmysql/link.go
@@ -2,15 +2,25 @@ package linkmysql
 
 import (
 	"database/sql"
+	"fmt"
+	"mysql_exporter/config"
 
 	"github.com/sirupsen/logrus"
 )
 
-func LinkDB(dsn string) *sql.DB {
+func LinkDB(options config.MySql) (*sql.DB, error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s",
+		options.UserName,
+		options.Password,
+		options.Host,
+		options.Port,
+		options.DB)
 	// 链接 mysql
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		logrus.Fatal(err)
+		return nil, err
 	}
-	return db
+	return db, nil
 }
