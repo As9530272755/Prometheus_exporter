8f04945e70f5dac5538c64e185ab1954ca865e6a
Code:Z <58386845+As9530272755@users.noreply.github.com>
2021-10-09 14:17:25 +0800

    Update log.go

--- mysql_exporter/logs/log.go
+++ mysql_exporter/logs/log.go
@@ -2,32 +2,36 @@ package logs
 
 import (
 	"fmt"
+	"mysql_exporter/config"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 // 定义日志信息
-func MysqlLog(fileName, level string, max_age, max_size, max_backups int, compress bool) {
+func MysqlLog(options config.Log) func() {
 
 	logger := lumberjack.Logger{
-		Filename:   fileName,
-		MaxAge:     max_age,
-		MaxSize:    max_size,
-		MaxBackups: max_backups,
-		Compress:   compress,
+		Filename:   options.FileName,
+		MaxAge:     options.Max_age,
+		MaxSize:    options.Max_size,
+		MaxBackups: options.Max_backups,
+		Compress:   options.Compress,
 	}
 
-	defer logger.Close()
-
 	// 解析 level 传入的日志级别
-	logLevel, err := logrus.ParseLevel(level)
+	logLevel, err := logrus.ParseLevel(options.Level)
 	fmt.Println(logLevel, err)
 
 	logrus.SetOutput(&logger)
 
 	logrus.SetLevel(logLevel)
 	logrus.SetReportCaller(true)
+
+	// 返回一个 close
+	return func() {
+		logger.Close()
+	}
 }
 
 // 定义日志记录监控项
