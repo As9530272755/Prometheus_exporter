3811e5c341d32a435462718692855ec6be6ab6cf
Code:Z <58386845+As9530272755@users.noreply.github.com>
2022-07-21 00:14:24 +0800

    Update main.go

--- ns_exporter/main.go
+++ ns_exporter/main.go
@@ -7,11 +7,14 @@ import (
 	"kube_expoter/msgErr"
 	"net/http"
 
+	_ "github.com/CodyGuo/godaemon"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	//mkconfigDir.MakeDir()
+	mux := http.NewServeMux()
 
 	options, err := ex_config.ParseConfig("./etc/exporter.yaml")
 	msgErr.ErrInfo(err)
@@ -20,6 +23,6 @@ func main() {
 
 	controller.PrometheusCotroller(options)
 
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(addr, nil)
+	mux.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(addr, mux)
 }
